6f6972796d933030338817a2f382b06dfb7d7049
kostyakozko <kostyakozko@hotmail.com>
2015-02-24 03:27:27 +0200

    Implemented dummy android app that closes #2

--- Android/src/com/nl/mobilevotingsystem/MainActivity.java
+++ Android/src/com/nl/mobilevotingsystem/MainActivity.java
@@ -1,36 +1,54 @@
 package com.nl.mobilevotingsystem;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+	private Socket server;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //following should be moved to onClick buttons
+    	SendMessage sendMessageTask = new SendMessage();
+    	sendMessageTask.execute(); 
     }
-
-
+    
+    //class should have input params
+    private class SendMessage extends AsyncTask<Void, Void, Void> {
+    	@Override
+    	//as well as this function
+    	protected Void doInBackground(Void... params) {
+	    	try {
+	    	 	server = new Socket("192.168.1.4", 12345);
+	    	 	//wait is for testing only, should be removed
+	    	    try {
+	    	    	Thread.sleep(60000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	    		server.close(); // closing the connection
+	    	 
+	    	} catch (UnknownHostException e) {
+	    		e.printStackTrace();
+	    	} catch (IOException e) {
+	    		e.printStackTrace();
+	    	}
+	    	return null;
+    	} 
+    }
+    
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
