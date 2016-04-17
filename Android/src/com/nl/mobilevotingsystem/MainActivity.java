9289178b97e3edb95cc90f8ba8c0236a5eccd3a3
kostyakozko <kostyakozko@hotmail.com>
2016-04-17 23:09:38 +0300

    Android app has buttons that send info. resolved #4

--- Android/src/com/nl/mobilevotingsystem/MainActivity.java
+++ Android/src/com/nl/mobilevotingsystem/MainActivity.java
@@ -7,6 +7,8 @@ import java.net.UnknownHostException;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends Activity {
 
@@ -15,26 +17,34 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        //following should be moved to onClick buttons
+        //following should be moved to onClick buttons 
+    }
+    
+    public void onVote(View v) {
     	SendMessage sendMessageTask = new SendMessage();
-    	sendMessageTask.execute(); 
+    	switch(v.getId()) {
+    	case R.id.buttonYes:
+    		sendMessageTask.execute(1);
+    		break;
+    	case R.id.buttonNeutral:
+    		sendMessageTask.execute(0);
+    		break;
+    	case R.id.buttonNo:
+    		sendMessageTask.execute(-1);
+    		break;
+    	}
     }
     
     //class should have input params
-    private class SendMessage extends AsyncTask<Void, Void, Void> {
+    private class SendMessage extends AsyncTask<Integer, Void, Void> {
     	@Override
     	//as well as this function
-    	protected Void doInBackground(Void... params) {
+    	protected Void doInBackground(Integer... params) {
 	    	try {
 	    	 	server = new Socket("192.168.1.4", 12345);
-	    	 	//wait is for testing only, should be removed
-	    	    try {
-	    	    	Thread.sleep(60000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+	    	 	server.getOutputStream().write(params[0]);
 	    		server.close(); // closing the connection
 	    	 
 	    	} catch (UnknownHostException e) {
